c643930bb0147c90ff425e3a295530f23a5e788a
alena0915 <X.li-49@student.tudelft.nl>
2020-04-28 13:27:11 +0200

    UI sterotype

---
+++ 3D_COnstruction/3D_COnstruction/_3Dpre_cb.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArcGIS.Core.CIM;
+using ArcGIS.Core.Data;
+using ArcGIS.Core.Geometry;
+using ArcGIS.Desktop.Catalog;
+using ArcGIS.Desktop.Core;
+using ArcGIS.Desktop.Editing;
+using ArcGIS.Desktop.Extensions;
+using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+using ArcGIS.Desktop.Framework.Dialogs;
+using ArcGIS.Desktop.Framework.Threading.Tasks;
+using ArcGIS.Desktop.Mapping;
+
+namespace _3D_COnstruction
+{
+    /// <summary>
+    /// Represents the ComboBox
+    /// </summary>
+    internal class _3Dpre_cb : ComboBox
+    {
+
+        private bool _isInitialized;
+
+        /// <summary>
+        /// Combo Box constructor
+        /// </summary>
+        public _3Dpre_cb()
+        {
+            UpdateCombo();
+         
+        }
+
+        /// <summary>
+        /// Updates the combo box with all the items.
+        /// </summary>
+
+        private void UpdateCombo()
+        {
+            // TODO – customize this method to populate the combobox with your desired items  
+            if (_isInitialized)
+                SelectedItem = ItemCollection.FirstOrDefault(); //set the default item in the comboBox
+
+
+            if (!_isInitialized)
+            {
+                Clear();
+                this.Add(new ComboBoxItem("2D Footprint"));
+                this.Add(new ComboBoxItem("3D OBJ Mesh"));
+                this.Add(new ComboBoxItem("CityJSON"));
+
+                _isInitialized = true;
+            }
+
+
+            Enabled = true; //enables the ComboBox
+            SelectedItem = ItemCollection.FirstOrDefault(); //set the default item in the comboBox
+
+        }
+
+        /// <summary>
+        /// The on comboBox selection change event. 
+        /// </summary>
+        /// <param name="item">The newly selected combo box item</param>
+        protected override void OnSelectionChange(ComboBoxItem item)
+        {
+
+            if (item == null)
+                return;
+
+            if (string.IsNullOrEmpty(item.Text))
+                return;
+
+            // TODO  Code behavior when selection changes.    
+        }
+
+    }
+}
---
+++ Add-in_IFC/Add-in_IFC/ProWindow1.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Add_in_IFC
+{
+    /// <summary>
+    /// Interaction logic for ProWindow1.xaml
+    /// </summary>
+    public partial class ProWindow1 : ArcGIS.Desktop.Framework.Controls.ProWindow
+    {
+        public ProWindow1()
+        {
+            InitializeComponent();
+            cb_MLA.SelectedIndex = 1;
+            cb_MLA.Items.Add("Hierarchical Fusion");
+            cb_MLA.Items.Add("Neural Network");
+            cb_0.IsChecked = true;
+            cb_1.IsChecked = true;
+            cb_2.IsChecked = true;
+        
+        }
+
+    }
+}
